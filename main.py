d06d9728c3df54e991ad95ec20ff46b14469fd31
patrikas159 <patrikas.limantas@stud.kitm.lt>
2021-03-12 08:51:40 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+#1 Dalis:
+
+#1. Sukurti bent 4 skirtingus data tipus.
+
+#2. Sukurti 3 skirtingas data struktūras su minimum 3 įrašais ir atvaizduoti kiekvieną įraša į atskira eilute.
+
+#3. Kokiu stilium kuriami python kintamieji? pvz: MANO_KINTAMASIS = 1, Mano_Kintamasis = 2 ir t.t.
+
+#4. Kaip sukurti kintamajį reprezentuojantį float data tipą?
+
+#5. Parašyti funkcija kuri visada gražins įrašus iš sarašo padavus bet kokį sarašą kaip parametrą.
+
+print("Hello")
+#-----------------
+x = int(3.5)
+print(x)
+#------------------
+
+print(float(10))
+#--------------------
+c1 = 3 + 6
+c2 = 6 + 15
+print(c1 + c2)
+#-------------------
+
+my_list = [1, 2, 3]
+for element in my_list:
+    print(element)
+#---------------
+x = 10
+print(x + 1)
+#-------------
+x = float("10")
+
+print(x-1)
+#------2-dalis-------------
+#6. Kokias reikšmes turės x ir y kintamieji?
+#kintamasis = "IlgasSakinys"
+#x = kintamasis[2:5]
+#y = kintamasis[7:9]
+
+#7. Kuo skiriasi mutable nuo not mutable data strukturos ir data tipai?
+
+#8. Jeigu norėtumėm patobulinti mūsų funkcija kuri išspauzdina sarašo įrašus žinant, kad type(kintamasis) gražina kintamojo data tipą parašyti funkcija kuri patikrina ar parametras funkcijoje yra list ar dictionary ir išspauzdinti jų reikšmes.
+
+#-------
+# x= kintamasis[10]
+# y = kintamasis[63]
+#------------------
+# not mutable galoi buti keiciami po sukurimo,o mutable negalis
