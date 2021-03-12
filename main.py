ee3413316f03340ac673b80237f76e0e81032314
patrikas159 <patrikas.limantas@stud.kitm.lt>
2021-03-12 09:27:43 +0200

    naujas

--- main.py
+++ main.py
@@ -9,30 +9,34 @@
 #4. Kaip sukurti kintamajį reprezentuojantį float data tipą?
 
 #5. Parašyti funkcija kuri visada gražins įrašus iš sarašo padavus bet kokį sarašą kaip parametrą.
+#--1
+print("Hello");
 
-print("Hello")
-#-----------------
 x = int(3.5)
-print(x)
-#------------------
+print(x);
+
 
 print(float(10))
-#--------------------
+
 c1 = 3 + 6
 c2 = 6 + 15
-print(c1 + c2)
-#-------------------
+print(c1 + c2);
+
+
+#---2
 
 my_list = [1, 2, 3]
 for element in my_list:
     print(element)
-#---------------
-x = 10
-print(x + 1)
-#-------------
+#----3
+kintamasis="reiksme"
+#----4
 x = float("10")
-
 print(x-1)
+#-----5
+pirmas = ['ruduo', 'ziema']
+antras = ["pavasaris", "vasara"]
+print(pirmas)
 #------2-dalis-------------
 #6. Kokias reikšmes turės x ir y kintamieji?
 #kintamasis = "IlgasSakinys"
@@ -43,8 +47,8 @@ print(x-1)
 
 #8. Jeigu norėtumėm patobulinti mūsų funkcija kuri išspauzdina sarašo įrašus žinant, kad type(kintamasis) gražina kintamojo data tipą parašyti funkcija kuri patikrina ar parametras funkcijoje yra list ar dictionary ir išspauzdinti jų reikšmes.
 
-#-------
-# x= kintamasis[10]
-# y = kintamasis[63]
-#------------------
-# not mutable galoi buti keiciami po sukurimo,o mutable negalis
+#---6
+x='gas'
+y='ki'
+#-----7
+# not mutable gali buti keiciami po sukurimo,o mutable negali
